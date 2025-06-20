37dc9a87672ca5a4022f4b4aa4707d3835a659a0
BrunoMantovan <brunochupetin1@gmail.com>
2025-06-20 09:37:10 -0300

    added an adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
+
   integrations: [tailwind(), preact()],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 });
